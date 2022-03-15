c995fcc848e2a742c9e960614719d392d33a82cc
nasubibocchi <matsuhito125@gmail.com>
2022-03-15 19:34:10 +0900

    Create query object

---
+++ app/queries/fetch_todos_query.rb
@@ -0,0 +1,21 @@
+class FetchTodosQuery
+    include Query
+
+    attr_reader :body, :isDone
+
+    validates :body, :presence :true
+    validates :isDone, :presence :true
+
+    def call
+        todos = fetch_todos
+        todo_list = ::TodoList.new(todos: todos)
+
+        self.result = todo_list
+    end
+
+    private
+
+        def fetch_todos
+            ::Todo.includes(:body, :isDone).order(updated_at: :desc)
+        end
+end
