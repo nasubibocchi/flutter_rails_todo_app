af2110f96a1d877476578d16fe9b0c019a4f5fd8
nasubibocchi <matsuhito125@gmail.com>
2022-04-02 21:42:41 +0900

    Create todos controller test

---
+++ spec/requests/todos_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe ::TodosController, type: :request do
+  describe "#GET todos/all" do
+    let(:url) { '/todos/all' }
+    let(:headers) do
+      {
+        'Content-Type': 'application/json',
+      }
+    end
+
+    subject { get url, headers: headers }
+
+    context "There are todo list to return" do
+      before do
+        todos_arr = []
+        5.times do
+          todos_arr << build(:todo)
+        end
+        ::Todo.import!(todos_arr)
+      end
+
+      it 'returns 200' do
+        subject
+        binding.irb
+        expect(response.status).to eq 200
+
+        res = JSON.parse(response.body)
+        expect(res['todos'].count).to eq 5
+      end
+    end
+  end
+end
