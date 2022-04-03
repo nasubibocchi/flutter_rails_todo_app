7fa874928537967fe10aac5a8360444a3abb917b
nasubibocchi <matsuhito125@gmail.com>
2022-04-03 18:07:41 +0900

    Modify create todo method

--- app/command/create_todo_command.rb
+++ app/command/create_todo_command.rb
@@ -1,21 +1,18 @@
 class CreateTodoCommand
     include Command
     attr_reader :body, :isDone
-    attr_accessor :todos
+    attr_accessor :todo
 
     validates :body, presence: true
     validates :isDone, presence: true
 
     def initialize(body:, isDone:)
-        @body = body
+        @body = before_destroy
         @isDone = isDone
     end
 
     def run
-        todo = ::Todo.create_with_detail!(
-            body: body,
-            isDone: isDone,
-        )
-        self.todos = todo;
+        todo = ::Todo.create_todo!( body: body, isDone: isDone )
+        self.todo = todo
     end
 end
