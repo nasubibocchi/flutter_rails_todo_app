50e33a0bc84f29f0ca538b9e2278bcc48452613a
nasubibocchi <matsuhito125@gmail.com>
2022-04-04 17:59:25 +0900

    Fix parameter

--- spec/requests/todos_controller_spec.rb
+++ spec/requests/todos_controller_spec.rb
@@ -22,12 +22,37 @@ describe ::TodosController, type: :request do
 
       it 'returns 200' do
         subject
-        binding.irb
         expect(response.status).to eq 200
 
         res = JSON.parse(response.body)
         expect(res['todos'].count).to eq 5
       end
+    end
+  end
+
+  describe "#POST todos" do
+    let(:url) { '/todos' }
+    let(:headers) do
+      {
+        'Content-Type': 'application/json',
+      }
+    end
+
+    let(:params) {}
+
+    subject { post url, params: params.to_json, headers: headers }
+
+    let(:params) do
+      {
+        body: 'body',
+        isDone: 'isDone',
+      }
+    end
+
+    it 'creates a record' do
+      binding.irb
+      expect { subject }.to change { ::Todo.count }.by(1)
+      expect(response.status).to eq 200
     end
   end
 end
