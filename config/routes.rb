d19707ee0179de720b6e5c82d5fcb991606606c5
nasubibocchi <matsuhito125@gmail.com>
2022-04-09 15:28:17 +0900

    Delete unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # get '/isDone', to: 'todos#isDone'
-  # get '/body', to: 'todos#body'
-  # resources :todos
-
-  get "/todos", to: "todos#index"
-  # get "/todos/:id", to: "todos#show"
-
   resources :todos, only: [] do
     collection do
       get :all
