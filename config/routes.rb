a77be6a2f71e3a41fd806e8295d88d1c37fb683b
nasubibocchi <matsuhito125@gmail.com>
2022-06-20 12:56:57 +0900

    Add update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :todos, only: [:create] do
+  resources :todos, only: [:create, :update] do
     collection do
       get :all
     end
