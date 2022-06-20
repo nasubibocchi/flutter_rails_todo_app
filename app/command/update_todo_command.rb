54093599a446cfd6f337d8db44d94a6cb04c88d9
nasubibocchi <matsuhito125@gmail.com>
2022-06-20 13:37:11 +0900

    Fix update todo command

--- app/command/update_todo_command.rb
+++ app/command/update_todo_command.rb
@@ -1,19 +1,23 @@
 class UpdateTodoCommand
     include Command
 
-    attr_reader :body, :is_done
-    attr_accessor :todos
+    attr_reader :id, :body, :is_done
+    attr_accessor :todo
 
+    validates :id, presence: true
     validates :body, presence: true
     validates :is_done, inclusion: { in: [true, false] }
 
-    def initialize(name:, is_done:)
+    def initialize(id:, body:, is_done:)
+        @id = id
         @body = body
-        @isDone = is_done
+        @is_done = is_done
+
+        @todo = ::Todo.find_by(id: id)
     end
 
     def run
-        todos.detail.update!(body: body, isDone: is_done)
-        self.todos = todos
+        todo.update!(id: id, body: body, is_done: is_done)
+        self.todo = todo
     end
 end
