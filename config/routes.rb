807f538c547f6a987c52a14d6f91ffa7ef202781
nasubibocchi <matsuhito125@gmail.com>
2022-06-20 13:59:32 +0900

    Add destroy todo routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :todos, only: [:create, :update] do
+  resources :todos, only: [:create, :update, :destroy] do
     collection do
       get :all
     end
