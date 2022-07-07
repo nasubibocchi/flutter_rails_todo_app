c175f40b15e7946f12a3a9ede64db1817916a18c
nasubibocchi <matsuhito125@gmail.com>
2022-07-07 14:48:57 +0900

    Validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'user validation' do
+    context 'when user has valid email' do
+      let(:user) { create(:user, name: 'MyString', email: 'abcde@email.com') }
+      it 'should be valid' do
+        expect(user.valid?).to eq true
+      end
+    end
+  end
 end
