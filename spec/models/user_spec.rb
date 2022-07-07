b06c96f76535bebdacf5220042eb0108c9954def
nasubibocchi <matsuhito125@gmail.com>
2022-07-07 15:14:14 +0900

    Test only when user is valid

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,12 +8,5 @@ RSpec.describe User, type: :model do
         expect(user.valid?).to eq true
       end
     end
-
-    context 'when user has invalid email' do
-      let(:user) { create(:user, name: 'MyString', email: 'abcde@email..com') }
-      it 'should be valid' do
-        expect(user.valid?).to eq false
-      end
-    end
   end
 end
