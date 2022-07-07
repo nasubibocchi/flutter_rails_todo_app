36bffcc021a2f4caa40d8e7bb20915d6b6e4dcb7
nasubibocchi <matsuhito125@gmail.com>
2022-07-07 17:18:24 +0900

    Add password validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'user validation' do
     context 'when user has valid email' do
-      let(:user) { create(:user, name: 'MyString', email: 'abcde@email.com') }
+      let(:user) { create(:user,
+                            name: 'MyString',
+                            email: 'abcde@email.com',
+                            password: 'foobar',
+                            password_confirmation: 'foobar') }
       it 'should be valid' do
         expect(user.valid?).to eq true
       end
