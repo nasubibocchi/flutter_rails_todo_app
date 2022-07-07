58d67706c91a827b2e91f19eb434a2fd79615b7e
nasubibocchi <matsuhito125@gmail.com>
2022-07-07 16:53:37 +0900

    Add password to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_07_055503) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_07_075016) do
   create_table "todos", force: :cascade do |t|
     t.string "body"
     t.boolean "is_done", default: false, null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_07_055503) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
